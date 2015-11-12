6d8903cb910f0a2036e80dafc0e5da0dfecf3f98
a104641811 <13414149714@163.com>
2015-11-12 16:41:43 +0800

    Initial Commit

---
+++ keyboardTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  keyboardTestDemo
+//
+//  Created by Superman on 15/11/12.
+//  Copyright © 2015年 dwd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ keyboardTestDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  keyboardTestDemo
+//
+//  Created by Superman on 15/11/12.
+//  Copyright © 2015年 dwd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
